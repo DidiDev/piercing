0972a33116c4f1182ec77e66c4aee16291dd0482
Reyan Ghazzaoui <ghazzaoui.reyan@gmail.com>
2025-06-04 15:18:15 +0200

    init

---
+++ wp-content/themes/tatoo-lite/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Tatoo Lite
+ */
+?>
+</div><!-- main-aligner -->
+
+<div class="mentions">
+    <div class="aligner">
+        <a href="http://localhost/php/wordpress/mentions-legales">Mentions légales</a>
+    </div>
+</div>
+    <div class="copyright-wrapper">
+        <div class="aligner">
+            <div class="copyright">
+                <p>Copyright 2025 - Créé par <a href="https://twireweb.fr/">TwireWeb</a></p>
+            </div><!-- copyright --><div class="clear"></div>           
+        </div><!-- aligner -->
+    </div>
+        
+<?php wp_footer(); ?>
+
+</body>
+</html>
